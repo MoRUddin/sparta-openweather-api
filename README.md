304c8f4c653b962d9292da0551f6e222346f36c4
Tech-A22 <tech-a22@Tech-A22s-MacBook-Air.local>
2018-03-23 00:05:12 +0000

    initial readme, tests ready to be written, class and service structure linked and testable

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # sparta-openweather-api
+
+## Task
+To read an API response from the open weather [API](https://openweathermap.org/) into a class with testable methods, splitting the class into two main services: sigle city search, and multiple city search.
+
+## Requirements
+* Ruby (built in 5.1.5)
+* The rspec gem
+* The JSON gem
+* The HTTParty gem
+
+## How to run tests
+* `git clone` the HTML link from [here](https://github.com/MoRUddin/Sparta-JSON-Parsing) to download the project to your local environment
+* run `bundle` to install the rspec, json, and httparty gems
+* run `rspec` on the project folder to run the tests
---
+++ spec/open_weather_spec.rb
@@ -0,0 +1,69 @@
+require 'spec_helper'
+
+describe OpenWeather do
+
+  context 'requesting information on a single weather works correctly' do
+
+    before(:all) do
+      @open_weather = OpenWeather.new.single
+      @response = @open_weather.query_id('2643743') #input a city id
+    end
+
+    it "should respond with a coordinates hash" do
+      expect(@open_weather.co_ord).to be_a(Hash)
+    end
+
+    it "should respond with a lattitude string" do
+      expect(@open_weather.co_ord["lat"]).to be_a(Float)
+    end
+
+    it "should respond with a longitude string" do
+      expect(@open_weather.co_ord["lon"]).to be_a(Float)
+    end
+
+    it "should respond with a weather array" do
+      expect(@open_weather.weather).to be_an(Array)
+    end
+
+    it "should respond with a weather array should contain a hash with 4 elements inside it" do
+      expect(@open_weather.weather[0].length).to eq(4)
+    end
+
+    it "should respond with the base 'stations'" do
+      expect(@open_weather.base).to eq("stations")
+    end
+
+    it "should respond with a main hash with 5 elements in it" do
+      expect(@open_weather.main.length).to eq(5)
+    end
+
+    it "should respond with a visibility integer" do
+      expect(@open_weather.visibility).to be_an(Integer)
+    end
+
+    it "should respond with a wind hash" do
+      expect(@open_weather.wind).to be_a(Hash)
+    end
+
+    it "should respond with a wind hash with 2 elements in it" do
+      expect(@open_weather.wind.length).to eq(2)
+    end
+
+    it "should respond with a clouds hash" do
+      expect(@open_weather.clouds).to be_a(Hash)
+    end
+  end
+
+  context "multiple weather validation" do
+
+    before(:all) do
+      @open_weather = OpenWeather.new.multiple
+      @response = @open_weather.query_ids("524901,703448,2643743") #input city ids
+    end
+
+    it "should respond with a status message of 200" do
+      expect(@open_weather.name(2)).to eq("London")
+    end
+  end
+
+end
